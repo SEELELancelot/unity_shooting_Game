8c7466292a4a3e06dc9fd71571ae264ce149abb6
SEELE_global <flegact60132@gmail.com>
2018-09-03 17:10:55 +0800

    insert Player.cs rocket.cs

---
+++ Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private float move_x;
+    private float move_z;
+    public float speed=1;
+    protected Transform m_transform;
+    public Transform m_rocket;
+
+    public float can_insert_time;
+    private float time=0;
+    // Use this for initialization
+	void Start () {
+        m_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        move_x = 0;
+        move_z = 0;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            move_z -= speed*Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            move_z += speed * Time.deltaTime;          
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move_x += speed * Time.deltaTime;       
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move_x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            if (time >= can_insert_time)
+            {
+                Instantiate(m_rocket, m_transform.position, m_transform.rotation);
+                time = 0;
+            }
+        }
+        m_transform.Translate(new Vector3(move_x, 0, move_z));
+    }
+}
---
+++ rocket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+    public float rocket_speed=-1;
+    public float rocket_life_time=3;
+    public float rocket_power = 1;
+    protected Transform m_transform;
+	// Use this for initialization
+	void Start () {
+        m_transform = this.transform;
+        Destroy(this.gameObject,rocket_life_time);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_transform.Translate(new Vector3(0,0,rocket_speed*Time.deltaTime));
+	}
+}
